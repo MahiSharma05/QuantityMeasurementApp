bccd200e3f7507c009fc9c85db42ea9337153592
MahiSharma05 <sharmama0905@gmail.com>
2026-03-09 22:56:25 +0530

    [Mahi Sharma] Add. UC10 unit interface for multicategory support completed.

---
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -0,0 +1,14 @@
+package com.apps.quantitymeasurement;
+
+public interface IMeasurable {
+	
+	public double getConversionFactor();
+	
+	public double convertToBaseUnit(double value);
+	
+	public double convertFromBaseUnit(double baseValue);
+	
+	public static void main(String[] args) {
+		System.out.println("IMeasurable Interface!");	
+	}
+}
